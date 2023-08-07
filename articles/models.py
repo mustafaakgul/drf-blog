3dd4c5245a7c099ccb26d430b2fba99e90b4638f
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-08-07 12:53:55 +0300

    upd articles model

--- articles/models.py
+++ articles/models.py
@@ -1,21 +1,12 @@
 from django.db import models
-#from django_blog.apps.account.models import User
-#from django_blog.apps.common.models import CoreModel
+from common.models.core import CoreModel
+from tags.models import Tag
+from django.contrib.auth.models import User
 
 
-"""
-class Post(CoreModel):
+class Article(CoreModel):
+    author = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    text = models.TextField()
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
-    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, blank=True, null=True)
-    image = models.ImageField(null=True, blank=True)
-"""
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    text = models.TextField()
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
-    #author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, blank=True, null=True)
-    image = models.ImageField(null=True, blank=True)
+    text = models.TextField(max_length=1000)
+    tags = models.ManyToManyField(Tag, related_name='articles', blank=True)
+    image = models.ImageField(upload_to='article/', null=True, blank=True)
