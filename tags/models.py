ad5e04bb4650701921a1ef37284b896bc8e93011
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-08-07 15:02:32 +0300

    upd tag model

--- tags/models.py
+++ tags/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from core.models.core import CoreModel
+from autoslug import AutoSlugField
 
 
-class Tag(CoreModel):
+class Tag(CoreModel): # Category
     name = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='name', unique=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.slug
